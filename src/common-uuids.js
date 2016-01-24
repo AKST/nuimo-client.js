b96a49228b3ec89069c41132ed90b3630d8ab18e
Angus Thomsen <aksthomsen@gmail.com>
2016-01-24 10:16:48 +1000

    adds fly and matrix uuids

--- src/common-uuids.js
+++ src/common-uuids.js
@@ -1,15 +1,35 @@
 export const SERVICE_SENSOR_UUID       = "f29b1525cb1940f3be5c7241ecb82fd2";
+export const SERVICE_MATRIX_UUID       = "f29b1523cb1940f3be5c7241ecb82fd1";
 export const CHARACTERISTIC_CLICK_UUID = "f29b1529cb1940f3be5c7241ecb82fd2";
-export const CHARACTERISTIC_SWIPE_UUID = "f29b1527cb1940f3be5c7241ecb82fd2";
 export const CHARACTERISTIC_TURN_UUID  = "f29b1528cb1940f3be5c7241ecb82fd2";
+export const CHARACTERISTIC_SWIPE_UUID = "f29b1527cb1940f3be5c7241ecb82fd2";
+export const CHARACTERISTIC_FLY_UUID   = "f29b1526cb1940f3be5c7241ecb82fd2";
+
+export const serviceMeta = {
+  [SERVICE_SENSOR_UUID]: "sensor",
+  [SERVICE_MATRIX_UUID]: "matrix",
+};
+
+export const characteristicMeta = {
+  [CHARACTERISTIC_CLICK_UUID]: "click",
+  [CHARACTERISTIC_SWIPE_UUID]: "swipe",
+  [CHARACTERISTIC_TURN_UUID]: "turn",
+  [CHARACTERISTIC_FLY_UUID]: "fly",
+};
 
 export default {
   services: {
     sensor: SERVICE_SENSOR_UUID,
+    matrix: SERVICE_MATRIX_UUID,
   },
   characteristics: {
+    fly: CHARACTERISTIC_FLY_UUID,
     click: CHARACTERISTIC_CLICK_UUID,
     swipe: CHARACTERISTIC_SWIPE_UUID,
     turn: CHARACTERISTIC_TURN_UUID,
+  },
+  meta: {
+    characteristicMeta,
+    serviceMeta,
   },
 };
