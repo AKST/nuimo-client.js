918cc6b82f9060091747c17a45b760856989f933
Hans Bakker <hansmbakker@Hanss-Mac-mini.local>
2016-04-09 17:01:09 +0200

    export FlyUpdate

--- src/index.ts
+++ src/index.ts
@@ -4,4 +4,4 @@ export { default as NuimoClient } from "./client/nuimoClient";
 
 export { withNuimo, withNuimos } from "./client/clientFactory";
 
-export { Update, ClickUpdate, SwipeUpdate, TurnUpdate } from "./update/update";
+export { Update, ClickUpdate, SwipeUpdate, TurnUpdate, FlyUpdate } from "./update/update";
